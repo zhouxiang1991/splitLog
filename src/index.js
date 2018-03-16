cf8bed0d6eb0cc7865691f8cbe9a9bf5a0e5eeb3
zhouxiang1991 <zhouxiang1991@caifujilu.com>
2018-03-16 16:36:20 +0800

    initial

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import {
+  execSync,
+} from 'child_process';
+import {
+  statSync,
+} from 'fs';
+import {
+  CronJob,
+} from 'cron';
+
+export default (cronTime = '0 0 0 * * *') => {
+  const job = new CronJob({
+    cronTime,
+    onTick() {
+      const out = statSync('./logs/out/out.log');
+      if (
+        out
+        && out.size
+      ) {
+        execSync('cp ./logs/out/out.log ./logs/out/$(date +"%Y-%m-%d").log');
+        execSync('> ./logs/out/out.log');
+      }
+      const err = statSync('./logs/err/err.log');
+      if (
+        err
+        && err.size
+      ) {
+        execSync('cp ./logs/err/err.log ./logs/err/$(date +"%Y-%m-%d").log');
+        execSync('> ./logs/err/err.log');
+      }
+    },
+    start: false,
+    timeZone: 'Asia/Shanghai',
+  });
+  return job;
+};
